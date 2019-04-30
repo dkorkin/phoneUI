826ab7602895d85fc3dc271128540b03ce22221c
Dmitriy Korkin <dkorkin12@gmail.com>
2019-04-30 14:04:50 +0300

    initial

---
+++ TestPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPhone
+//
+//  Created by Dmitriy Korkin on 30/04/2019.
+//  Copyright © 2019 Dmitriy Korkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
